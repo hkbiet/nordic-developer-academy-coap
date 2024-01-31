7f22a1b1260c3546c2f80561fe6660d6a8453428
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-01-31 17:14:49 +0100

    feat: add server cert

--- server/main.go
+++ server/main.go
@@ -7,12 +7,30 @@ import (
 	"log"
 	"time"
 	"flag"
+	"os"
+	"path"
+	"crypto/tls"
+	"context"
+	"crypto/x509"
 
+	piondtls "github.com/pion/dtls/v2"
+	"github.com/plgd-dev/go-coap/v3/dtls"
 	coap "github.com/plgd-dev/go-coap/v3"
 	"github.com/plgd-dev/go-coap/v3/message"
 	"github.com/plgd-dev/go-coap/v3/message/codes"
 	"github.com/plgd-dev/go-coap/v3/mux"
+	"github.com/plgd-dev/go-coap/v3/examples/dtls/pki"
+	"github.com/plgd-dev/go-coap/v3/options"
+	"github.com/plgd-dev/go-coap/v3/net"
+	"github.com/plgd-dev/go-coap/v3/udp/client"
 )
+
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
 func loggingMiddleware(next mux.Handler) mux.Handler {
 	return mux.HandlerFunc(func(w mux.ResponseWriter, r *mux.Message) {
@@ -73,10 +91,38 @@ func dynamicResource(w mux.ResponseWriter, r *mux.Message) {
 }
 
 func main() {
-	address := flag.String("address", "localhost:5688",
-		"The UDP Server listen address with port, e.g. `:5688` or `0.0.0.0:5688`.")
+	address := flag.String("address", "localhost",
+		"The UDP Server listen address, e.g. `localhost` or `0.0.0.0`.")
+	certDir := flag.String("certDir", "/home/coap", "The folder containing the server certificate.")
+	udpAddr := fmt.Sprintf("%s:%d", *address, 5688)
+	dtlsAddr := fmt.Sprintf("%s:%d", *address, 5689)
 	flag.Parse()
-	fmt.Printf("UDP Server listening on: %s\n", *address)
+	fmt.Printf("UDP Server listening on: %s\n", udpAddr)
+	fmt.Printf("dTLS UDP Server listening on: %s\n", dtlsAddr)
+
+	// Read certificate files
+	caCert, err := os.ReadFile(path.Join(*certDir, "CA.crt"))
+    check(err)
+	cert, err := os.ReadFile(path.Join(*certDir, "server.crt"))
+    check(err)
+	key, err := os.ReadFile(path.Join(*certDir, "server.key"))
+    check(err)
+	certificate, err := pki.LoadKeyAndCertificate(key, cert)
+	check(err)
+	certPool, err := pki.LoadCertPool(caCert)
+	check(err)
+
+	ctx := context.Background()
+
+	tlsConfig := &piondtls.Config{
+		Certificates:         []tls.Certificate{*certificate},
+		ExtendedMasterSecret: piondtls.RequireExtendedMasterSecret,
+		ClientCAs:            certPool,
+		ClientAuth:           piondtls.RequireAndVerifyClientCert,
+		ConnectContextMaker: func() (context.Context, func()) {
+			return context.WithTimeout(ctx, 30*time.Second)
+		},
+	}
 
 	r := mux.NewRouter()
 	r.Use(loggingMiddleware)
@@ -85,6 +131,32 @@ func main() {
 
 	log.Println("Server starting")
 
-	log.Fatal(coap.ListenAndServe("udp", *address, r))
+	log.Fatal(coap.ListenAndServe("udp", udpAddr, r))
+	log.Fatal(listenAndServeDTLS("udp", dtlsAddr, tlsConfig, r))
+}
 
+
+func listenAndServeDTLS(network string, addr string, config *piondtls.Config, handler mux.Handler) error {
+	l, err := net.NewDTLSListener(network, addr, config)
+	if err != nil {
+		return err
+	}
+	defer l.Close()
+	s := dtls.NewServer(options.WithMux(handler), options.WithOnNewConn(onNewConn))
+	return s.Serve(l)
+}
+
+func onNewConn(cc *client.Conn) {
+	dtlsConn, ok := cc.NetConn().(*piondtls.Conn)
+	if !ok {
+		log.Fatalf("invalid type %T", cc.NetConn())
+	}
+	clientCert, err := x509.ParseCertificate(dtlsConn.ConnectionState().PeerCertificates[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	cc.SetContextValue("client-cert", clientCert)
+	cc.AddOnClose(func() {
+		log.Println("closed connection")
+	})
 }
