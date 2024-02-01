2fca4d1632d1bc9724d8a4b3752e39136678a44d
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-02-01 16:41:12 +0100

    ci: get logs

--- server/start.sh
+++ server/start.sh
@@ -5,3 +5,4 @@ IP=`ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
 
 nohup /home/coap/coap-server -address "${IP}" -password "connect:anything" -dTLS &
 nohup /home/coap/coap-server -address "${IP}" &
+tail -f nohup.out
