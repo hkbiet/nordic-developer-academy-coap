97d9a337bc80ae4ae90920ec02249ea1808b1858
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-02-01 16:59:46 +0100

    fix: run in bg

--- server/start.sh
+++ server/start.sh
@@ -3,5 +3,5 @@
 # we need to figure out the IP of the public interface here
 IP=`ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
 
-nohup /home/coap/coap-server -address "${IP}" -password "connect:anything" -dTLS &
-nohup /home/coap/coap-server -address "${IP}" &
+/home/coap/coap-server -address "${IP}" -password "connect:anything" -dTLS &
+/home/coap/coap-server -address "${IP}" &
