b8006f82b7d455e21162eea5cfe5fd2f6403e155
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-02-01 17:08:01 +0100

    ci: sleep forever

--- server/start.sh
+++ server/start.sh
@@ -3,5 +3,6 @@
 # we need to figure out the IP of the public interface here
 IP=`ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
 
-/home/coap/coap-server -address "${IP}" -password "connect:anything" -dTLS &
-/home/coap/coap-server -address "${IP}" &
+nohup /home/coap/coap-server -address "${IP}" -password "connect:anything" -dTLS &
+nohup /home/coap/coap-server -address "${IP}" &
+sleep infinity
