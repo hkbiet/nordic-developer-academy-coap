7845fd3fed9733583721305e853ed17fc938e6b3
Fredrik Wikestad <fredrik.wikestad@atea.no>
2024-02-09 14:32:47 +0100

    enable secrets

--- server/start.sh
+++ server/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-#export STORAGE_CONNECTION_STRING=$(cat /run/secrets/STORAGE_CONNECTION_STRING)
+FILE=/run/secrets/STORAGE_CONTAINER_STRING     
+if [ -f $FILE ]; then
+export STORAGE_CONNECTION_STRING=$(cat /run/secrets/STORAGE_CONNECTION_STRING)
+fi
 
 # we need to figure out the IP of the public interface here
 IP=`ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
