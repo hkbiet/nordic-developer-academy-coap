54d92ac2adcf51f8e14ef3d922cbf5762da6aef2
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-04-03 09:55:05 +0200

    fix(ipv6): format addresses

--- server/server.go
+++ server/server.go
@@ -7,9 +7,9 @@ import (
 	"os"
 
 	"coap-server/internal"
+
 	piondtls "github.com/pion/dtls/v2"
 	coap "github.com/plgd-dev/go-coap/v3"
-
 
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 )
@@ -26,6 +26,7 @@ func main() {
 	password := flag.String("password", "connect:anything",
 		"The password to use for the PSK in dTLS.")
 	dtls := flag.Bool("dTLS", false, "Start a dTLS server")
+	udp6 := flag.Bool("udp6", false, "Whether to use IPv6")
 	flag.Parse()
 
 	storageConnectionString, ok := os.LookupEnv("STORAGE_CONNECTION_STRING")
@@ -44,11 +45,25 @@ func main() {
 
 	r := internal.NewServer(storageClient, containerName)
 
+	udpPort := 5688
+	dTLSPort := 5689
+	var udpAddr string
+	var dtlsAddr string
+	var network string
+	if *udp6 {
+		udpAddr = fmt.Sprintf("[%s]:%d", *address, udpPort)
+		dtlsAddr = fmt.Sprintf("[%s]:%d", *address, dTLSPort)
+		network = "udp6"
+	} else {
+		udpAddr = fmt.Sprintf("%s:%d", *address, udpPort)
+		dtlsAddr = fmt.Sprintf("%s:%d", *address, dTLSPort)
+		network = "udp"
+	}
+
 	if *dtls {
-		dtlsAddr := fmt.Sprintf("%s:%d", *address, 5689)
 		log.Printf("dTLS UDP Server listening on: %s\n", dtlsAddr)
 		log.Printf("dTLS PSK: %s\n", *password)
-		log.Fatal(coap.ListenAndServeDTLS("udp", dtlsAddr, &piondtls.Config{
+		log.Fatal(coap.ListenAndServeDTLS(network, dtlsAddr, &piondtls.Config{
 			PSK: func(hint []byte) ([]byte, error) {
 				log.Printf("Client's hint: %s \n", hint)
 				return []byte(*password), nil
@@ -57,8 +72,7 @@ func main() {
 			CipherSuites:    []piondtls.CipherSuiteID{piondtls.TLS_PSK_WITH_AES_128_CCM_8},
 		}, r))
 	} else {
-		udpAddr := fmt.Sprintf("%s:%d", *address, 5688)
 		log.Printf("UDP Server listening on: %s\n", udpAddr)
-		log.Fatal(coap.ListenAndServe("udp", udpAddr, r))
+		log.Fatal(coap.ListenAndServe(network, udpAddr, r))
 	}
 }
