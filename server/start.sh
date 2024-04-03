f2579e44545e327b2b3e2500126e95351fbf4922
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-04-03 10:27:05 +0200

    fix(start): add support for IPv6

--- server/start.sh
+++ server/start.sh
@@ -5,15 +5,26 @@ if [ -f $FILE ]; then
 export STORAGE_CONNECTION_STRING=$(cat /run/secrets/STORAGE_CONNECTION_STRING)
 fi 
 
-# Enumerate all network interfaces and get their assigned IP addresses
-interfaces=$(ip -o addr show | awk '{print $2}' | cut -d':' -f1)
+# Enumerate all IPv4 network interfaces and get their assigned IP addresses
+ipv4_interfaces=$(ip -o -4 addr show | awk '{print $2}' | cut -d':' -f1)
 
-for interface in $interfaces; do
-    IP=$(ip -o addr show $interface | awk '{print $4}' | cut -d'/' -f1)
+for interface in $ipv4_interfaces; do
+    IP=$(ip -o -4 addr show $interface | awk '{print $4}' | cut -d'/' -f1)
     echo "Interface: $interface, Assigned IP: $IP"
 
     nohup /home/coap/coap-server -address "${IP}" -password "connect:anything" -dTLS &
     nohup /home/coap/coap-server -address "${IP}" &
+done
+
+# Enumerate all IPv6 network interfaces and get their assigned IP addresses
+ipv6_interfaces=$(ip -o -6 addr show | awk '{print $2}' | cut -d':' -f1)
+
+for interface in $ipv6_interfaces; do
+    IP=$(ip -o -6 addr show $interface | awk '{print $4}' | cut -d'/' -f1)
+    echo "Interface: $interface, Assigned IPv6: $IP"
+
+    nohup /home/coap/coap-server -address "${IP}" -ipv6 -password "connect:anything" -dTLS &
+    nohup /home/coap/coap-server -address "${IP}" -ipv6 &
 done
 
 sleep infinity
